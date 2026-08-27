e4e8ecbaca7a5538d246c1175f84bbdad9058b07
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2026-08-27 14:47:52 +0300

    py_bazauti: The OG version - python

--- rust_app/src/bin/bazaplay.rs
+++
@@ -1,8 +0,0 @@
-use rust_app::audio_parser::wav_parser::WAVParser;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut parser = WAVParser::new("audio_files/842503__icheytc__william-blake-london.wav");
-    let _ = parser.parse()?;
-    parser.render();
-    Ok(())
-}
